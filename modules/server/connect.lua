c8f1a0aca5c0c3e8446401997e08576c3a694fd9
EinS4ckZwiebeln <schultzfabian@gmx.de>
2022-12-12 19:57:56 +0100

    fix: ip lookup

--- modules/server/connect.lua
+++ modules/server/connect.lua
@@ -1,4 +1,6 @@
 AddEventHandler("playerConnecting", function (playerName, setKickReason, deferrals)
+    source = tonumber(source)
+    local ipv4 = Util.GetIdentifier(source, "ip:"):sub(4)
     if ServerConfig.Modules.Connect.NameFilter.enabled or ServerConfig.Modules.Connect.NoVPN.enabled then
         deferrals.defer()
         Citizen.Wait(0)
@@ -9,7 +11,7 @@ AddEventHandler("playerConnecting", function (playerName, setKickReason, deferra
     end
 
     if ServerConfig.Modules.Connect.NoVPN.enabled then
-        PerformHttpRequest("https://blackbox.ipinfo.app/lookup/" .. GetPlayerEndpoint(source), function(errorCode, usingVPN, resultHeaders)
+        PerformHttpRequest("https://blackbox.ipinfo.app/lookup/" .. ipv4, function(errorCode, usingVPN, resultHeaders)
             if not usingVPN == "N" then
                 deferrals.done()
             else
