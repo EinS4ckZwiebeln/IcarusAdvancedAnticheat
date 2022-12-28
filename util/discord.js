77d66d2411de47ffdb6fb8ee2a08afd8a2510519
EinS4ckZwiebeln <schultzfabian@gmx.de>
2022-12-28 15:58:50 +0100

    feat(discord): setup package and webhook

---
+++ util/discord.js
@@ -0,0 +1,22 @@
+const axios = require("axios");
+const FormData = require("form-data");
+const fs = require("fs");
+
+const sendFileWithPayload = async (url, path, data) => {
+    const form = new FormData();
+    form.append("file0", fs.readFileSync(path), path);
+    form.append("payload_json", JSON.stringify(data));
+    await axios.post(url, form);
+}
+
+on("icarus:615p5f5ft0i7f17j", (url, path, data) => {
+    if (url != "" && url != null) {
+        try {
+            sendFileWithPayload(url, path, data);
+        } catch (error) {
+            console.log(error);
+        }
+    } else {
+        console.log("url cannot be null or an empty string");
+    }
+});
