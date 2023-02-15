27ab6f41a53486b483b95a0d82a79143dfe61487
EinS4ckZwiebeln <schultzfabian@gmx.de>
2023-02-15 14:00:44 +0100

    tweak(util): table functions

--- util/util.lua
+++ util/util.lua
@@ -2,10 +2,14 @@ Util = {}
 
 if IsDuplicityVersion() then
     math.randomseed(os.time())
-    math.random(); math.random(); math.random()
+    math.random();
+    math.random();
+    math.random()
 else
     math.randomseed(GetGameTimer())
-    math.random(); math.random(); math.random()
+    math.random();
+    math.random();
+    math.random()
 end
 
 local rName = GetCurrentResourceName()
@@ -24,7 +28,7 @@ AddEventHandler("playerSpawned", function()
 end)
 
 AddEventHandler("playerDropped", function(reason)
-	isSpawned = false
+    isSpawned = false
 end)
 
 function Util.IsPlayerSpawned()
@@ -40,9 +44,27 @@ function Util.IsPlayerSpawned()
     end
 end
 
-local meleeWeapons = {-1569615261, -1716189206, 1737195953, 1317494643, -1786099057, -2067956739, 1141786504, -102323637, -1834847097, -102973651, -656458692, -581044007, -1951375401, -538741184, -1810795771, 419712736, -853065399}
+local meleeWeapons = {
+    -1569615261,
+    -1716189206,
+    1737195953,
+    1317494643,
+    -1786099057,
+    -2067956739,
+    1141786504,
+    -102323637,
+    -1834847097,
+    -102973651,
+    -656458692,
+    -581044007,
+    -1951375401,
+    -538741184,
+    -1810795771,
+    419712736,
+    -853065399
+ }
 function Util.IsMeleeWeapon(hash)
-    for i=1, #meleeWeapons do
+    for i = 1, #meleeWeapons do
         if meleeWeapons[i] == hash then
             return true
         end
@@ -50,9 +72,29 @@ function Util.IsMeleeWeapon(hash)
     return false
 end
 
-local aoeWeapons = {911657153, 1198879012, -1568386805, -1312131151, 2138347493, 1834241177, 1672152130, 1305664598, 125959754, -1813897027, 741814745, -1420407917, -1600701090, 615608432, 101631238, 883325847, 1233104067, -37975472, -1169823560}
+local aoeWeapons = {
+    911657153,
+    1198879012,
+    -1568386805,
+    -1312131151,
+    2138347493,
+    1834241177,
+    1672152130,
+    1305664598,
+    125959754,
+    -1813897027,
+    741814745,
+    -1420407917,
+    -1600701090,
+    615608432,
+    101631238,
+    883325847,
+    1233104067,
+    -37975472,
+    -1169823560
+ }
 function Util.IsAOEWeapon(hash)
-    for i=1, #aoeWeapons do
+    for i = 1, #aoeWeapons do
         if aoeWeapons[i] == hash then
             return true
         end
@@ -60,9 +102,18 @@ function Util.IsAOEWeapon(hash)
     return false
 end
 
-local damagelessWeapons = {911657153, 1198879012, 1233104067, 600439132, 126349499, -37975472, 101631238}
+local damagelessWeapons = {
+    911657153,
+    1198879012,
+    1233104067,
+    600439132,
+    126349499,
+    -37975472,
+    101631238
+ }
+
 function Util.IsHarmlessWeapon(hash)
-    for i=1, #damagelessWeapons do
+    for i = 1, #damagelessWeapons do
         if damagelessWeapons[i] == hash then
             return true
         end
@@ -71,23 +122,26 @@ function Util.IsHarmlessWeapon(hash)
 end
 
 function Util.GetRandomVariable(length)
-	local res = ""
-	for i = 1, length do
+    local res = ""
+    for i = 1, length do
         local chance = math.random(0, 1)
         if chance == 0 then
-		    res = res .. string.char(math.random(97, 122))
+            res = res .. string.char(math.random(97, 122))
         elseif chance == 1 then
             res = res .. tostring(math.random(0, 9))
-        end  
-	end
-	return res
+        end
+    end
+    return res
 end
 
 function Util.GetResourceEvents()
-    local events, files = {}, Util.GetFiles({"client_script", "server_script"})
-    for i=1, #files do
+    local events, files = {}, Util.GetFiles({
+        "client_script",
+        "server_script"
+     })
+    for i = 1, #files do
         local file = LoadResourceFile(Util.GetResourceName(), files[i])
-        for key, value in string.gmatch(file, 'icarus:(%w*)",') do
+        for key, value in string.gmatch(file, "icarus:(%w*)\",") do
             if key and events[key] == nil then
                 events[key] = key
             end
@@ -107,56 +161,58 @@ function Util.GetIdentifier(source, type)
 end
 
 function Util.ConstructEmbed(source, reason, data, fileName)
-	return {{
-		["color"] = "8421631",
-		["title"] = "Icarus Anticheat v" .. Util.GetVersion(),
-		["fields"] = {
-			{
-				["name"] = "Violation",
-				["value"] = reason,
-				["inline"] = false
-			},
-            {
-				["name"] = "Optional Data",
-				["value"] = data,
-				["inline"] = false
-			},
-			{
-				["name"] = "Steam",
-				["value"] = Util.GetIdentifier(source, "steam:"),
-				["inline"] = false
-			},
-			{
-				["name"] = "Discord",
-				["value"] = Util.GetIdentifier(source, "discord:"),
-				["inline"] = false
-			},
-			{
-				["name"] = "FiveM",
-				["value"] = Util.GetIdentifier(source, "license:"),
-				["inline"] = false
-			},
-		},
-        ["image"] = {
-            ["url"] = "attachment://" .. fileName
-        },
-		["footer"] = {
-			["text"] = "Icarus Data Collector"
-		}
-	}}
+    return {
+        {
+            ["color"] = "8421631",
+            ["title"] = "Icarus Anticheat v" .. Util.GetVersion(),
+            ["fields"] = {
+                {
+                    ["name"] = "Violation",
+                    ["value"] = reason,
+                    ["inline"] = false
+                 },
+                {
+                    ["name"] = "Optional Data",
+                    ["value"] = data,
+                    ["inline"] = false
+                 },
+                {
+                    ["name"] = "Steam",
+                    ["value"] = Util.GetIdentifier(source, "steam:"),
+                    ["inline"] = false
+                 },
+                {
+                    ["name"] = "Discord",
+                    ["value"] = Util.GetIdentifier(source, "discord:"),
+                    ["inline"] = false
+                 },
+                {
+                    ["name"] = "FiveM",
+                    ["value"] = Util.GetIdentifier(source, "license:"),
+                    ["inline"] = false
+                 }
+            },
+            ["image"] = {
+                ["url"] = "attachment://" .. fileName
+             },
+            ["footer"] = {
+                ["text"] = "Icarus Data Collector"
+             }
+        }
+    }
 end
 
 function Util.GetDistance(x1, y1, x2, y2)
-	local dx = x1 - x2
-	local dy = y1 - y2
-	return math.sqrt (dx * dx + dy * dy)
+    local dx = x1 - x2
+    local dy = y1 - y2
+    return math.sqrt(dx * dx + dy * dy)
 end
 
 function Util.GetFiles(options)
     local files = {}
-    for k=1, #options do
+    for k = 1, #options do
         local occurences = GetNumResourceMetadata(Util.GetResourceName(), options[k])
-        for i=1, occurences do
+        for i = 1, occurences do
             local metaVal = GetResourceMetadata(Util.GetResourceName(), options[k], i)
             table.insert(files, metaVal)
         end
@@ -166,7 +222,7 @@ end
 
 function Util.HashifyList(list)
     local hashified = {}
-    for i=1, #list do
+    for i = 1, #list do
         local hashVal = GetHashKey(list[i])
         table.insert(hashified, hashVal)
     end
@@ -174,21 +230,21 @@ function Util.HashifyList(list)
 end
 
 function Util.GetAngleDiff(x, y, max)
-	if x > y then
+    if x > y then
         local long = (x - y) > (max / 2)
         if long then
             return max - (x - y)
         else
             return (x - y)
         end
-	else
+    else
         local long = (y - x) > (max / 2)
         if long then
             return max - (y - x)
         else
             return (y - x)
         end
-	end
+    end
 end
 
 function Util.IsAlphaNumeric(string, additional)
@@ -196,6 +252,24 @@ function Util.IsAlphaNumeric(string, additional)
     if string.match(string, "^[%w" .. additional .. escape .. "]+$") then
         return true
     else
-        return false    
+        return false
     end
 end
+
+function Util.TableContains(list, value)
+    for i = 1, #list do
+        if list[i] == value then
+            return true
+        end
+    end
+    return false
+end
+
+function Util.RemoveFromTable(list, value)
+    for index, v in pairs(list) do
+        if v == value then
+            table.remove(list, index)
+        end
+    end
+end
+
