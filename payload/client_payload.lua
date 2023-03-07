e9148d109051ccc1fc5a3721495acc213374e5f7
EinS4ckZwiebeln <schultzfabian@gmx.de>
2023-03-07 10:00:29 +0100

    refactor(payload): add remaining coroutines

--- payload/client_payload.lua
+++ payload/client_payload.lua
@@ -239,6 +239,223 @@ Citizen.CreateThread(function()
 end)
 
 Citizen.CreateThread(function()
+    local textures = {
+        {
+            txd = "HydroMenu",
+            txt = "HydroMenuHeader",
+            name = "HydroMenu"
+         },
+        {
+            txd = "John",
+            txt = "John2",
+            name = "SugarMenu"
+         },
+        {
+            txd = "darkside",
+            txt = "logo",
+            name = "Darkside"
+         },
+        {
+            txd = "ISMMENU",
+            txt = "ISMMENUHeader",
+            name = "ISMMENU"
+         },
+        {
+            txd = "dopatest",
+            txt = "duiTex",
+            name = "Copypaste Menu"
+         },
+        {
+            txd = "fm",
+            txt = "menu_bg",
+            name = "Fallout"
+         },
+        {
+            txd = "wave",
+            txt = "logo",
+            name = "Wave"
+         },
+        {
+            txd = "wave1",
+            txt = "logo1",
+            name = "Wave (alt.)"
+         },
+        {
+            txd = "meow2",
+            txt = "woof2",
+            name = "Alokas66",
+            x = 1000,
+            y = 1000
+         },
+        {
+            txd = "adb831a7fdd83d_Guest_d1e2a309ce7591dff86",
+            txt = "adb831a7fdd83d_Guest_d1e2a309ce7591dff8Header6",
+            name = "Guest Menu"
+         },
+        {
+            txd = "hugev_gif_DSGUHSDGISDG",
+            txt = "duiTex_DSIOGJSDG",
+            name = "HugeV Menu"
+         },
+        {
+            txd = "MM",
+            txt = "menu_bg",
+            name = "MetrixFallout"
+         },
+        {
+            txd = "wm",
+            txt = "wm2",
+            name = "WM Menu"
+         },
+        {
+            txd = "absoluteeulen",
+            txt = "Absolut",
+            name = "Absolut Menu"
+         },
+        {
+            txd = "Dopamine",
+            txt = "Dopameme",
+            name = "Dopamine Menu"
+         },
+        {
+            txd = "SkidMenu",
+            txt = "skidmenu",
+            name = "Skid Menu"
+         },
+        {
+            txd = "tiago",
+            txt = "Tiago",
+            name = "Tiago Menu"
+         },
+        {
+            txd = "lynxmenu",
+            txt = "lynxmenu",
+            name = "Lynx Menu"
+         },
+        {
+            txd = "Reaper",
+            txt = "reaper",
+            name = "Reaper Menu"
+         },
+        {
+            txd = "NeekerMan",
+            txt = "NeekerMan1",
+            name = "Lumia Menu"
+         },
+        {
+            txd = "Blood-X",
+            txt = "Blood-X",
+            name = "Blood-X Menu"
+         },
+        {
+            txd = "Fallout",
+            txt = "FalloutMenu",
+            name = "Fallout Menu"
+         },
+        {
+            txd = "Luxmenu",
+            txt = "Lux meme",
+            name = "LuxMenu"
+         },
+        {
+            txd = "KekHack",
+            txt = "kekhack",
+            name = "KekHack Menu"
+         },
+        {
+            txd = "Maestro",
+            txt = "maestro",
+            name = "Maestro Menu"
+         },
+        {
+            txd = "Brutan",
+            txt = "brutan",
+            name = "Brutan Menu"
+         },
+        {
+            txd = "FiveSense",
+            txt = "fivesense",
+            name = "Fivesense Menu"
+         },
+        {
+            txd = "lynxrevolution",
+            txt = "revolution",
+            name = "Lynx Menu"
+         },
+        {
+            txd = "Hydramenu",
+            txt = "hydramenu",
+            name = "Hydra Menu"
+         },
+        {
+            txd = "Genesis",
+            txt = "Genesis",
+            name = "Genesis Menu"
+         },
+        {
+            txd = "Sugä",
+            txt = "Sugo",
+            name = "Sugar Menu"
+         },
+        {
+            txd = "Watermalone",
+            txt = "watermalone",
+            name = "Watermalone Menu"
+         }
+    }
+
+    while ClientConfig.Modules.DUITextures.enabled do
+        Citizen.Wait(30000)
+        for i, data in pairs(textures) do
+            if data.x and data.y then
+                if GetTextureResolution(data.txd, data.txt).x == data.x and GetTextureResolution(data.txd, data.txt).y == data.y then
+                    TriggerServerEvent("icarus:417szjzm1goy", "DUI Texture (" .. data.name .. ") [C1]", false)
+                    return
+                end
+            else
+                if GetTextureResolution(data.txd, data.txt).x ~= 4.0 then
+                    TriggerServerEvent("icarus:417szjzm1goy", "DUI Texture (" .. data.name .. ") [C2]", false)
+                    return
+                end
+            end
+        end
+
+    end
+end)
+
+Citizen.CreateThread(function()
+    while ClientConfig.Modules.SuperJump.enabled do
+        Citizen.Wait(200)
+
+        if IsPedDoingBeastJump(_G.PLAYER_PED) then
+            TriggerServerEvent("icarus:417szjzm1goy", "Superjump [C1]", false, {
+                beastJump = true
+             })
+            return
+        end
+
+        if IsPedJumping(_G.PLAYER_PED) then
+            local startPos = GetEntityCoords(_G.PLAYER_PED)
+            while IsPedJumping(_G.PLAYER_PED) do
+                Citizen.Wait(0)
+                if IsPedJumpingOutOfVehicle(_G.PLAYER_PED) or IsPedRagdoll(_G.PLAYER_PED) or IsPedInParachuteFreeFall(_G.PLAYER_PED) or GetPedParachuteState(_G.PLAYER_PED) > 0 then
+                    break
+                end
+            end
+
+            local endPos = GetEntityCoords(_G.PLAYER_PED)
+            local horizontalDist = Util.GetDistance(startPos.x, startPos.y, endPos.x, endPos.y)
+            if horizontalDist > 25.0 and not IsPedDeadOrDying(_G.PLAYER_PED) then
+                TriggerServerEvent("icarus:417szjzm1goy", "Superjump [C2]", false, {
+                    jumpLength = horizontalDist
+                 })
+                return
+            end
+        end
+    end
+end)
+
+Citizen.CreateThread(function()
     while true do
         Citizen.Wait(15000)
         local resourceList = {}
