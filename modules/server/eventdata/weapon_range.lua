14532093f444e86441869619b4f20547e677012e
EinS4ckZwiebeln <schultzfabian@gmx.de>
2023-03-13 21:00:31 +0100

    fix(weapon_range): adapt net event inconsistencies

--- modules/server/eventdata/weapon_range.lua
+++ modules/server/eventdata/weapon_range.lua
@@ -7,26 +7,25 @@ end
 local tazer = 911657153
 
 function WeaponRange.ProcessEventData(sender, data)
-    local rawTargetData, rawHashData = data[16], data[32]
-    if not rawTargetData or not rawHashData then return end
+    local rawTargetData = data["hitGlobalId"] or data["hitGlobalIds"][1]
+    local ped, target = GetPlayerPed(sender), NetworkGetEntityFromNetworkId(rawTargetData)
 
-    local ped = GetPlayerPed(sender)
-    local target, hash = GetPlayerPed(rawTargetData), rawHashData
-
-    if ped and target then
+    if DoesEntityExist(target) and IsPedAPlayer(target) then
         local pCoords = GetEntityCoords(ped)
-        local tCoords = GertEntityCoords(target)
+        local tCoords = GetEntityCoords(target)
         local dist = Util.GetDistance(pCoords.x, pCoords.y, tCoords.x, tCoords.y)
 
         if dist > 400.0 then
-            TriggerEvent("icarus:my602oxd71pv", sender, "Weapon Range [C1]", false, {range = dist})
+            TriggerEvent("icarus:my602oxd71pv", sender, "Weapon Range [C1]", false, {
+                range = dist
+             })
             CancelEvent()
         end
-        if hash == tazer and dist > ServerConfig.Modules.WeaponRange.maxTazerRange then
+        if data["weaponType"] == tazer and dist > ServerConfig.Modules.WeaponRange.maxTazerRange then
             TriggerEvent("icarus:my602oxd71pv", sender, "Weapon Range [C2]", false, {
-                tazedRange = dist, 
+                tazedRange = dist,
                 maxRange = ServerConfig.Modules.WeaponRange.maxTazerRange
-            })
+             })
             CancelEvent()
         end
     end
