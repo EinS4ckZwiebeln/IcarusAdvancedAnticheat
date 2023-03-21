8d4e45be258b3ed5d11c428cadf61711ee1e3e72
EinS4ckZwiebeln <schultzfabian@gmx.de>
2023-03-21 10:34:35 +0100

    refactor(update): deflate code and highlight update msg

--- core/server/update.js
+++ core/server/update.js
@@ -1,48 +1,22 @@
-const https = require("https");
+const currentVersion = GetResourceMetadata(curName, "version", 0);
 
-const currentVersion = GetResourceMetadata(GetCurrentResourceName(), "version", 0);
-
-const getLatestGithubRelease = (user, repo) => {
-    const options = {
-        hostname: "api.github.com",
-        port: 443,
-        path: `/repos/${user}/${repo}/releases`,
-        headers: {
-            "User-Agent": "request",
-        },
-    };
-    let release;
-    return new Promise((resolve, reject) => {
-        const request = https.request(options, (res) => {
-            let response = "";
-            res.on("data", (d) => {
-                response += d;
-            });
-            res.on("end", () => {
-                const releases = JSON.parse(response);
-                if (releases.length > 0) {
-                    release = releases[0].name;
-                    resolve(release);
-                } else {
-                    reject(new Error("No releases found"));
-                }
-            });
-        });
-        request.end();
-    });
+const config = {
+    method: "get",
+    headers: {
+        "User-Agent": "request"
+    },
 };
 
-function checkForUpdates() {
-    const user = "EinS4ckZwiebeln";
-    const repo = "IcarusAdvancedAnticheat";
-    getLatestGithubRelease(user, repo).then((release) => {
-        release = release.slice(1);
-        if (release != currentVersion) {
-            console.log("This version of Icarus is outdated. Please update to the latest version!\nLatest Version: " + release + " | Current Version: v" + currentVersion + "\nhttps://github.com/EinS4ckZwiebeln/IcarusAdvancedAnticheat/releases\n");
-        }
-    }).catch((err) => {
-        console.error(err);
-    });
+async function fetchLatestRelease() {
+    const request = await axios.get("https://api.github.com/repos/EinS4ckZwiebeln/IcarusAdvancedAnticheat/releases", config);
+    return request.data[0].name.toString().slice(1);
+}
+
+async function checkForUpdates() {
+    const latestRelease = await fetchLatestRelease();
+    if (currentVersion != latestRelease) {
+        console.log(`^1This version of Icarus is outdated. Please update to the latest version!\nLatest Version: ${latestRelease} | Current Version: ${currentVersion}`);
+    }
 }
 
 checkForUpdates();
