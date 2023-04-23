9d96e8a2a0c4ccac5dee8f0f6d19430d4ca1eaa4
EinS4ckZwiebeln <schultzfabian@gmx.de>
2023-04-23 11:46:41 +0200

    optimize&fix(explosions): use set and emit

--- modules/server/eventdata/explosions.js
+++ modules/server/eventdata/explosions.js
@@ -1,14 +1,16 @@
+const whitelistedExplosionTypes = new Set(serverConfig.Modules.ExplosionFilter.whitelistedExplosionTypes);
+
 module.exports = (sender, data) => {
-    if (!serverConfig.Modules.ExplosionFilter.whitelistedExplosionTypes.includes(data.explosionType)) {
-        emitNet("icarus:my602oxd71pv", sender, "Explosion Event [C1]", false, {
+    if (!whitelistedExplosionTypes.has(data.explosionType)) {
+        emit("icarus:my602oxd71pv", sender, "Explosion Event [C1]", false, {
             type: data.explosionType
         });
     } else if (data.damageScale > 1.0) {
-        emitNet("icarus:my602oxd71pv", sender, "Explosion Event [C2]", false, {
+        emit("icarus:my602oxd71pv", sender, "Explosion Event [C2]", false, {
             scale: data.damageScale
         });
     } else if (data.isInvisible) {
-        emitNet("icarus:my602oxd71pv", sender, "Explosion Event [C3]", false, {
+        emit("icarus:my602oxd71pv", sender, "Explosion Event [C3]", false, {
             invisible: data.isInvisible
         });
     }
