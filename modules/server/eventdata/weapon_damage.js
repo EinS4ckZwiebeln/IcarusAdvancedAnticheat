eca9f0ac486735a13622298fd24914b09ecbe009
EinS4ckZwiebeln <schultzfabian@gmx.de>
2023-04-23 14:51:45 +0200

    optimize(weapon_damage): use set instead of array for lookup

--- modules/server/eventdata/weapon_damage.js
+++ modules/server/eventdata/weapon_damage.js
@@ -1,8 +1,8 @@
-const hashedWeapons = hashify(serverConfig.BlacklistedWeapons);
+const hashedWeapons = new Set(hashify(serverConfig.BlacklistedWeapons));
 
 module.exports = (sender, data) => {
     const weaponHash = data.weaponType;
-    if (hashedWeapons.includes(weaponHash)) {
+    if (hashedWeapons.has(weaponHash)) {
         emit("icarus:my602oxd71pv", sender, "Illegal Weapon Damage [C1]", false, { weapon: weaponHash });
         CancelEvent();
     }
