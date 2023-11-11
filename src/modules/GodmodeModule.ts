da60ed68781bceede5c6c8cc1f00f187366aa8cd
EinS4ckZwiebeln <schultzfabian@gmx.de>
2023-11-11 17:34:19 +0100

    Simplify godmode logic

--- src/modules/GodmodeModule.ts
+++ src/modules/GodmodeModule.ts
@@ -1,15 +1,14 @@
 import { EventHandler } from "../core/handler/EventHandler";
 import { Module } from "../core/Module";
 import { Violation } from "../core/Violation";
-import { Utility } from "../util/Utility";
 
 export class GodmodeModule extends Module {
 	public onLoad(): void {
-		EventHandler.subscribe("weaponDamageEvent", (_: string, data: any) => this.onGodmode(data.hitGlobalId || data.hitGlobalIds[0], data.weaponType));
+		EventHandler.subscribe("weaponDamageEvent", (_: string, data: any) => this.onGodmode(data.hitGlobalId || data.hitGlobalIds[0]));
 	}
 
 	public onUnload(): void {
-		EventHandler.unsubscribe("weaponDamageEvent", (_: string, data: any) => this.onGodmode(data.hitGlobalId || data.hitGlobalIds[0], data.weaponType));
+		EventHandler.unsubscribe("weaponDamageEvent", (_: string, data: any) => this.onGodmode(data.hitGlobalId || data.hitGlobalIds[0]));
 	}
 
 	/**
@@ -17,15 +16,12 @@ export class GodmodeModule extends Module {
 	 * @param netId The network ID of the player.
 	 * @param weaponType The type of weapon used on the player.
 	 */
-	private onGodmode(netId: number, weaponType: number): void {
-		if (Utility.WEAPONS_NON_LETHAL.has(weaponType)) return;
+	private onGodmode(netId: number): void {
 		const target: number = NetworkGetEntityFromNetworkId(netId);
-		if (IsPedAPlayer(target)) {
-			if (GetPlayerInvincible(target.toString())) {
-				const violation = new Violation(source, "Godmode [C1]", this.name);
-				violation.banPlayer();
-				CancelEvent();
-			}
+		if (IsPedAPlayer(target) && GetPlayerInvincible(target.toString())) {
+			const violation = new Violation(source, "Godmode [C1]", this.name);
+			violation.banPlayer();
+			CancelEvent();
 		}
 	}
 }
