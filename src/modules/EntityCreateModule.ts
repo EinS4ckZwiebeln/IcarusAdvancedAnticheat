87ad1c583a89b1972beb6510db9fb9a2fb8ce386
EinS4ckZwiebeln <schultzfabian@gmx.de>
2023-11-26 19:21:45 +0100

    Refactor & optimize entity create module

--- src/modules/EntityCreateModule.ts
+++ src/modules/EntityCreateModule.ts
@@ -11,11 +11,12 @@ export class EntityCreateModule extends Module {
 	private _checkPedsForWeapons: boolean = false;
 
 	public onLoad(): void {
-		const models: string[] = Array.from(this.config.IllegalModels);
-		this._illegalEntities = new Set(Utility.hashify(models));
+		this._illegalEntities = new Set(Utility.hashify(this.config.IllegalModels));
 		this._blacklistedWeapons = new Set(Utility.hashify(this.config.BlacklistedWeapons));
+
 		this._banNetworkOwner = Config.getValue(this.config, "banNetworkOwner");
 		this._checkPedsForWeapons = Config.getValue(this.config, "checkPedsForWeapons");
+
 		EventHandler.subscribe("entityCreated", (entity: number) => this.onEntityCreated(entity));
 	}
 
@@ -29,38 +30,42 @@ export class EntityCreateModule extends Module {
 	 */
 	private onEntityCreated(entity: number): void {
 		if (!DoesEntityExist(entity)) return;
-
 		const owner: number = NetworkGetFirstEntityOwner(entity);
-		let violation: boolean = false;
 		// If the entity is illegal, ban the player.
 		if (this._illegalEntities.has(GetEntityModel(entity))) {
-			if (this._banNetworkOwner) {
-				const violation = new Violation(owner, "Illegal Entity [C1]", this.name);
-				violation.banPlayer();
-			}
-			violation = true;
-		}
-		// If the entity is attached to another entity and the owner of the attached entity is not the same as the owner of the entity, ban the player.
-		const rootEntity: number = GetEntityAttachedTo(entity);
-		if (rootEntity > 0 && NetworkGetFirstEntityOwner(rootEntity) != owner) {
-			if (this._banNetworkOwner) {
-				const violation = new Violation(owner, "Illegal Entity [C2]", this.name);
-				violation.banPlayer();
-			}
-			violation = true;
-		}
-		// If the entity is a ped and the owner is not a player and the selected weapon is blacklisted, ban the player.
-		if (this._checkPedsForWeapons && GetEntityType(entity) === 1 && !IsPedAPlayer(entity) && this._blacklistedWeapons.has(GetSelectedPedWeapon(entity))) {
-			if (this._banNetworkOwner) {
-				const violation = new Violation(owner, "Illegal Entity [C3]", this.name);
-				violation.banPlayer();
-			}
-			violation = true;
+			this.handleViolation("Illegal Entity [C1]", owner, entity);
+			return;
 		}
 
-		if (violation) {
-			DeleteEntity(entity);
-			CancelEvent();
+		// If the entity is attached to another entity and the owner of the attached entity is not the same as the owner of the entity, ban the player.
+		const rootEntity: number = GetEntityAttachedTo(entity);
+		if (rootEntity > 0 && NetworkGetFirstEntityOwner(rootEntity) !== owner) {
+			this.handleViolation("Illegal Entity [C2]", owner, entity);
+			return;
 		}
+
+		// If the entity is a ped and the owner is not a player and the selected weapon is blacklisted, ban the player.
+		if (this._checkPedsForWeapons) {
+			const weapon: number = GetSelectedPedWeapon(entity);
+			if (weapon > 0 && GetEntityType(entity) === 1 && this._blacklistedWeapons.has(weapon)) {
+				this.handleViolation("Illegal Entity [C3]", owner, entity);
+				return;
+			}
+		}
+	}
+
+	/**
+	 * Handles a violation by banning the network owner and deleting the entity.
+	 * @param violationType - The type of violation.
+	 * @param owner - The network owner of the entity.
+	 * @param entity - The entity to be deleted.
+	 */
+	private handleViolation(violationType: string, owner: number, entity: number): void {
+		if (this._banNetworkOwner) {
+			const violation = new Violation(owner, violationType, this.name);
+			violation.banPlayer();
+		}
+		DeleteEntity(entity);
+		CancelEvent();
 	}
 }
