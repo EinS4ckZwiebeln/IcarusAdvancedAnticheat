2a5eef86254d0c52633ecfd75c0be5b0b1e9264e
EinS4ckZwiebeln <schultzfabian@gmx.de>
2023-11-26 13:17:14 +0100

    Refactor eventhandler

--- src/core/handler/EventHandler.ts
+++ src/core/handler/EventHandler.ts
@@ -1,12 +1,15 @@
 import { Logger } from "../logger/Logger";
+
 /**
- * The EventHandler class provides a way to subscribe to and handle events.
+ * The EventHandler class provides a mechanism for subscribing to and triggering events.
+ * Events can be subscribed to using the `subscribe` method and unsubscribed from using the `unsubscribe` method.
+ * When an event is triggered, all subscribed callback functions are called.
  */
 export class EventHandler {
 	private static readonly _events: Map<string, Function[]> = new Map();
 	private static readonly _netEvents: Set<string> = new Set();
 
-	constructor() {
+	private constructor() {
 		throw new Error("EventHandler is a static class and cannot be instantiated.");
 	}
 
@@ -17,20 +20,9 @@ export class EventHandler {
 	 */
 	public static subscribe(eventName: string, callback: Function): void {
 		Logger.debug(`Subscribing to ${eventName}`);
-		if (!this._netEvents.has(eventName)) {
-			Logger.debug(`Creating ${eventName} EventHandler`);
-			onNet(eventName, async (...args: any[]) => {
-				// Call each callback
-				(this._events.get(eventName) || []).forEach((callback: Function) => {
-					callback(...args);
-				});
-			});
-			this._netEvents.add(eventName);
-		}
-
-		const eventCallbacks = this._events.get(eventName) || [];
-		eventCallbacks.push(callback);
-		this._events.set(eventName, eventCallbacks);
+		if (!this._netEvents.has(eventName)) this.registerNetEvent(eventName);
+		// Push new callback onto the stack
+		this._events.set(eventName, [...this.getEventCallbacks(eventName), callback]);
 	}
 
 	/**
@@ -40,8 +32,35 @@ export class EventHandler {
 	 */
 	public static unsubscribe(eventName: string, callback: Function): void {
 		Logger.debug(`Unsubscribing from ${eventName}`);
-		let eventCallbacks = this._events.get(eventName) || [];
-		eventCallbacks = eventCallbacks.filter((cb: Function) => cb !== callback);
+		const eventCallbacks = this.getEventCallbacks(eventName).filter((cb: Function) => cb !== callback);
 		this._events.set(eventName, eventCallbacks);
+	}
+
+	/**
+	 * Registers a network event with the specified event name.
+	 * @param eventName - The name of the network event to register.
+	 */
+	private static registerNetEvent(eventName: string): void {
+		Logger.debug(`Registering net event ${eventName}`);
+		onNet(eventName, async (...args: any[]) => this.triggerEventCallbacks(eventName, args));
+		this._netEvents.add(eventName);
+	}
+
+	/**
+	 * Retrieves the event callbacks for a given event name.
+	 * @param eventName The name of the event.
+	 * @returns An array of event callbacks.
+	 */
+	private static getEventCallbacks(eventName: string): Function[] {
+		return this._events.get(eventName) ?? [];
+	}
+
+	/**
+	 * Triggers the event callbacks for the specified event name with the given arguments.
+	 * @param eventName - The name of the event.
+	 * @param args - The arguments to pass to the event callbacks.
+	 */
+	private static triggerEventCallbacks(eventName: string, args: any[]): void {
+		this.getEventCallbacks(eventName).forEach((cb: Function) => cb(...args));
 	}
 }
