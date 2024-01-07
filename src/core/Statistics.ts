e0414a2a4f0eb1f154308a85f742d137f1c2d193
EinS4ckZwiebeln <66690565+EinS4ckZwiebeln@users.noreply.github.com>
2024-01-07 14:13:25 +0100

    Another fix yet again

--- src/core/Statistics.ts
+++ src/core/Statistics.ts
@@ -2,6 +2,7 @@ import { KvpStorage } from "../util/enum/KvpStorage";
 
 export class Statistics {
 	private static readonly _daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	private static readonly _currentdate = new Date();
 
 	private static _weeklyViolations: number[] = JSON.parse(GetResourceKvpString(KvpStorage.VIOLATIONS_WEEK) || "[]");
 	private static _sessionViolations: number = 0;
@@ -17,11 +18,10 @@ export class Statistics {
 	public static getWeeklyViolations(): { days: string[]; violations: number[] } {
 		const days: string[] = [];
 		const violations: number[] = [];
-		const currentDate = new Date();
 
 		for (let i = 6; i >= 0; i--) {
-			const localDate = new Date(currentDate);
-			localDate.setDate(currentDate.getDate() - i);
+			const localDate = new Date(this._currentdate);
+			localDate.setDate(this._currentdate.getDate() - i);
 
 			const dayOfWeek = this._daysOfWeek[localDate.getDay()];
 			const weekViolation = this._weeklyViolations[localDate.getDate()] || 0;
@@ -34,17 +34,9 @@ export class Statistics {
 	}
 
 	public static async incrementWeeklyViolations(): Promise<void> {
-		const currentDate = new Date();
-		const todaysDate = currentDate.getDate();
+		const todaysDate = this._currentdate.getDate();
 		this._weeklyViolations[todaysDate] = (this._weeklyViolations[todaysDate] || 0) + 1;
-		// Ensure that the kvp storage won't grow on forever
-		const saveWorthy = [];
-		for (let i = 6; i >= 0; i--) {
-			const localDate = new Date(currentDate);
-			localDate.setDate(currentDate.getDate() - i);
-			saveWorthy.push(this._weeklyViolations[localDate.getDate()] || 0);
-		}
-		SetResourceKvpNoSync(KvpStorage.VIOLATIONS_WEEK, JSON.stringify(saveWorthy));
+		SetResourceKvpNoSync(KvpStorage.VIOLATIONS_WEEK, JSON.stringify(this._weeklyViolations));
 		FlushResourceKvp();
 	}
 }
