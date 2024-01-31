0f2a79178f61b32de7d9b6a0f0e2dec1319fcc6d
EinS4ckZwieblen <schultzfabian@gmx.de>
2024-01-31 20:41:44 +0100

    Remove any from complex config type

--- src/types/ConfigType.ts
+++ src/types/ConfigType.ts
@@ -13,7 +13,15 @@ export type config = {
 	/**
 	 * An object containing module settings.
 	 */
-	Modules: any;
+	Modules: {
+		[key: string]: {
+			/**
+			 * Obviously there is more data being hold by a module,
+			 * but as of now there is no need to represent it here.
+			 */
+			enabled: boolean;
+		};
+	};
 	/**
 	 * An array of blacklisted weapon names.
 	 */
@@ -21,7 +29,9 @@ export type config = {
 	/**
 	 * An array of blacklisted event names.
 	 */
-	BlacklistedEvents: any;
+	BlacklistedEvents: {
+		[key: string]: string[];
+	};
 	/**
 	 * An array of illegal model names.
 	 */
