1a6760ac33f11af958c50eabfa5de129b0f20c63
EinS4ckZwieblen <schultzfabian@gmx.de>
2024-06-10 19:47:55 +0200

    Update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,21 +1,17 @@
 module.exports = {
 	preset: "ts-jest",
 	testEnvironment: "node",
-	globals: {
-		"ts-jest": {
-			importHelpers: true,
-			tsconfig: "tests/tsconfig.test.json",
-			diagnostics: {
-				warnOnly: true,
-				pathRegex: /\.(spec|test)\.ts$/,
-			},
-		},
-	},
-	// Remove this transform stuff once cfx finally updates to a newer version of node.js
-	/*
 	transform: {
-		"^.+\\.(ts|tsx|js|jsx)$": "esbuild-jest",
+		"^.+\\.tsx?$": [
+			"ts-jest",
+			{
+				importHelpers: true,
+				tsconfig: "tests/tsconfig.test.json",
+				diagnostics: {
+					warnOnly: true,
+				},
+			},
+		],
 	},
-	*/
 	transformIgnorePatterns: ["/node_modules/(?!axios)"],
 };
