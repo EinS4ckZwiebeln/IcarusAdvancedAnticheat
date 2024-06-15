93016655e9368cd260a6bc73cc205e633ee93cc3
EinS4ckZwiebeln <66690565+EinS4ckZwiebeln@users.noreply.github.com>
2024-06-15 12:42:19 +0200

    Add more tests

--- tests/unit/PermissionHandler.test.ts
+++ tests/unit/PermissionHandler.test.ts
@@ -4,13 +4,21 @@ import "../helper/MockConfig";
 import { Logger } from "../../src/core/logger/Logger";
 import { PermissionHandler } from "../../src/core/handler/PermissionHandler";
 import { container } from "tsyringe";
+import { Module } from "../../src/core/Module";
 
 Logger.init();
 const permissionHandler = container.resolve(PermissionHandler);
 
+class TestModule extends Module {
+	public onLoad(): void {}
+	public onUnload(): void {}
+}
+
 describe("PermissionHandler", () => {
+	const nativeCache = global.IsPlayerAceAllowed;
 	afterEach(() => {
 		permissionHandler["_permitted"].clear();
+		global.IsPlayerAceAllowed = nativeCache;
 	});
 	it("should return true if player has permission", () => {
 		const source = 1;
@@ -41,6 +49,30 @@ describe("PermissionHandler", () => {
 	it("should return false even if source is wrong type", () => {
 		const source = "1" as unknown as number;
 		const hasPermission = permissionHandler.hasPermission(source);
+		expect(hasPermission).toBe(false);
+	});
+	it("should return true if player has bypass ace permission", () => {
+		const source = 1;
+		const module = new TestModule();
+		global.IsPlayerAceAllowed = (_: string, perm: string) => {
+			if (perm === `icarus.${module.name.toLowerCase()}`) {
+				return true;
+			}
+			return false;
+		};
+		const hasPermission = permissionHandler.hasPermission(source, module.name);
+		expect(hasPermission).toBe(true);
+	});
+	it("should return false if player has no bypass ace permission", () => {
+		const source = 1;
+		const module = new TestModule();
+		global.IsPlayerAceAllowed = (_: string, perm: string) => {
+			if (perm === `icarus.${module.name.toLowerCase()}`) {
+				return true;
+			}
+			return false;
+		};
+		const hasPermission = permissionHandler.hasPermission(source, "NonExistentModule");
 		expect(hasPermission).toBe(false);
 	});
 });
