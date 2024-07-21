4f034492156d6b07125b0fbdb62515ce22a8022e
EinS4ckZwiebeln <66690565+EinS4ckZwiebeln@users.noreply.github.com>
2024-07-21 18:29:38 +0200

    Fix type error and add format options to module bypasses

--- src/core/handler/PermissionHandler.ts
+++ src/core/handler/PermissionHandler.ts
@@ -31,9 +31,46 @@ export class PermissionHandler {
 	 * @returns True if the player has permission, false otherwise.
 	 */
 	public hasPermission(source: number, module?: string): boolean {
-		source = parseInt(source.toString()); // Ensure source is really a number
-		const hasModuleBypass = module !== undefined ? IsPlayerAceAllowed(source.toString(), `icarus.${module.toLowerCase()}`) : false;
-		return this._permitted.has(source) || hasModuleBypass || IsPlayerAceAllowed(source.toString(), this._bypassPermission);
+		source = parseInt(source.toString()); // Ensure source is really a number during runtime
+		return (
+			this._permitted.has(source) ||
+			this.hasModuleBypass(source.toString(), module) ||
+			this.isPlayerAceAllowedBool(source.toString(), this._bypassPermission)
+		);
+	}
+
+	/**
+	 * Checks if a player has bypass permission for a specific module.
+	 * @param source - The source of the player.
+	 * @param module - The module to check for bypass permission. If not provided, returns false.
+	 * @returns True if the player has bypass permission for the module, false otherwise.
+	 */
+	private hasModuleBypass(source: string, module?: string): boolean {
+		if (module === undefined) return false;
+		const moduleLower = module.toLowerCase();
+		const formats = [`icarus.${moduleLower}`, `icarus.${moduleLower.slice(0, -"module".length)}_module`] as const;
+		for (const format of formats) {
+			if (this.isPlayerAceAllowedBool(source, format)) return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Checks if a player is allowed to perform a specific action based on their permission level.
+	 * @param source - The source of the player.
+	 * @param permission - The permission to check.
+	 * @returns A boolean indicating whether the player is allowed or not.
+	 */
+	private isPlayerAceAllowedBool(source: string, permission: string): boolean {
+		// Returns a number during connection deferring when the temp source is used
+		const allowed: boolean | number = IsPlayerAceAllowed(source, permission);
+		if (typeof allowed === "boolean") {
+			return allowed;
+		}
+		if (typeof allowed === "number") {
+			return allowed === 1;
+		}
+		return false;
 	}
 
 	/**
