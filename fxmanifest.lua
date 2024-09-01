c31dc553fcea5ebbd7938309589d8aaa659541cb
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 18:57:46 +0000

    chore: bump manifest version to v2.1.1

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game "gta5"
 
 author "EinS4ckZwiebeln"
 description "Efficient, modular server-side anticheat script."
-version "2.1.0"
+version "2.1.1"
 
 lua54 "yes"
 use_experimental_fxv2_oal "yes"
